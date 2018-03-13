0261c92310fefc40944b7480438e84eb82065de8
Sangreal8 <joshmalone@hotmail.com>
2018-03-13 12:33:13 +0000

    added partial intro function

---
+++ millionaire.py
@@ -0,0 +1,11 @@
+# CA278 Group Project: Who Wants to Be a Millionaire
+# Josh Malone: 15357971
+# Maksims Kompanijecs: 15306971
+# Michael Hubem: 15396501
+# Matthew Farrelly: 15366246
+
+def intro():
+   name = raw_input("Hello, welcome to Who Wants to be a Millionaire! What is your name?" + "\n")
+   print "Ok " + name + " this is how the game works:" + "\n"
+
+#intro()
