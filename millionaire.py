0c220b2008432282b71fac49f9d683faffc94919
Sangreal8 <joshmalone@hotmail.com>
2018-03-13 13:34:14 +0000

    completed intro function

--- millionaire.py
+++ millionaire.py
@@ -7,5 +7,9 @@
 def intro():
    name = raw_input("Hello, welcome to Who Wants to be a Millionaire! What is your name?" + "\n")
    print "Ok " + name + " this is how the game works:" + "\n"
-
-#intro()
+   print "You are asked a multiple choice question, with options A, B, C and D. If you get the correct answer, you earn money and move onto the next question. If you guess incorrectly then you lose the money you have earned, although there are check-points along the way that allow you to keep a certain amount of money should you get a question wrong further on." + "\n" +"The questions increase in difficulty as you progress but you can make use of the 3 'lifelines' we will give you:" 
+   print "1. 50/50" + "\n" + "This allows you to cut the possible answers from 4, down to 2."
+   print "2. Phone a Friend" + "\n" + "Place a call to a predetermined friend that will hopefully help you with an answer."
+   print "3. Ask the Audience" + "\n" "Our wonderful people (bots) in the audience will vote on the correct answer and we shall show you the results"
+   print "Ready to get started? Lets go!" + "\n" + "==================================================================="
+intro()
