1ab9f2a4b70ca26625d17d83243e568fe2512ebf
Sangreal8 <joshmalone@hotmail.com>
2018-03-13 14:45:28 +0000

    added prototype dictionary

--- questions_and_answers.py
+++ questions_and_answers.py
@@ -1,2 +1,11 @@
 # Think we should create a dictionary where we read in   #
 # the questions from the file and tie the correct answer to them  #
+import sys
+
+trivia = {}
+
+with open("test_questions", "r") as f:
+   for line in f:
+      tokens = line.split("/")
+      trivia[tokens[0]] = tokens[1].rsplit()
+print trivia 
