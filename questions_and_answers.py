73485ddb23c7e173a79c6da17fce92d63fa91d87
Sangreal8 <joshmalone@hotmail.com>
2018-03-13 15:31:47 +0000

    more work on formatting of questions and answers

--- questions_and_answers.py
+++ questions_and_answers.py
@@ -1,11 +1,18 @@
 # Think we should create a dictionary where we read in   #
 # the questions from the file and tie the correct answer to them  #
 import sys
+import random
 
 trivia = {}
 
 with open("test_questions", "r") as f:
    for line in f:
       tokens = line.split("/")
-      trivia[tokens[0]] = tokens[1].rsplit()
-print trivia 
+      trivia[tokens[0]] = tokens[1].rstrip()
+   
+   #print trivia
+   question = random.choice(list(trivia.items()))
+   answer = question[1]
+   question = question[0]
+   
+   print question, answer
