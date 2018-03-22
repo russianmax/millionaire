f4a6a4c05c828ade28a345ab9fc809eb37cfb623
Sangreal8 <joshmalone@hotmail.com>
2018-03-22 11:52:41 +0000

    re-implemented intro

--- millionaire.py
+++ millionaire.py
@@ -1,3 +1,19 @@
+# CA278 Group Project: Who Wants to Be a Millionaire
+# Josh Malone: 15357971
+# Maksims Kompanijecs: 15306971
+# Michael Hubem: 15396501
+# Matthew Farrelly: 15366246
+
+def intro():
+   name = raw_input("Hello, welcome to Who Wants to be a Millionaire! What is your name?" + "\n")
+   print "Ok " + name + " this is how the game works:" + "\n"
+   print "You are asked a multiple choice question, with options A, B, C and D. If you get the correct answer, you earn money and move onto the next question. If you guess incorrectly then you lose the money you have earned, although there are check-points along the way that allow you to keep a certain amount of money should you get a question wrong further on." + "\n" +"The questions increase in difficulty as you progress but you can make use of the 3 'lifelines' we will give you:" 
+   print "1. 50/50" + "\n" + "This allows you to cut the possible answers from 4, down to 2."
+   print "2. Phone a Friend" + "\n" + "Place a call to a predetermined friend that will hopefully help you with an answer."
+   print "3. Ask the Audience" + "\n" "Our wonderful people (bots) in the audience will vote on the correct answer and we shall show you the results"
+   print "Ready to get started? Lets go!" + "\n" + "==================================================================="
+
+
 answerlist = ["A","B","C","D", "Correct"]
 answerdict = {}
 f = open("question.txt")
